b80b20653ed53a82fbf973edce6b2857655bdf11
AtaScripts317 <hasanmusat3@gmail.com>
2026-09-20 21:14:04 +0300

    Add condition for PlaceId 17556820024

--- autoredirect/redirect.lua
+++ autoredirect/redirect.lua
@@ -1,4 +1,6 @@
 if game.PlaceId == 3297964905 then
+
+elseif game.PlaceId == 17556820024 then
   
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/Aimbot/Aimbot.lua", true))()
