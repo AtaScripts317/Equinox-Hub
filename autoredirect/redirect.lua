c89f92a83eb928ccea2a5315fd5b1ca38f7fb4aa
AtaScripts317 <hasanmusat3@gmail.com>
2026-09-20 22:53:27 +0300

    Update redirect.lua

--- autoredirect/redirect.lua
+++ autoredirect/redirect.lua
@@ -1,22 +1,22 @@
 local exec = "Unknown"
 
 pcall(function()
-	exec = identifyexecutor()
+    exec = identifyexecutor()
 end)
 
-if game.PlaceId == 3297964905 then return end
+if game.PlaceId == 3297964905 then
+    return
 
 elseif game.PlaceId == 17556820024 then
 
-	if type(exec) == "string" then
-		if exec:lower():find("real") then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/ufg/EqHubUFG.lua"))()
-	else
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/ufg/lowlevelExctrs.lua"))()
-	end
-
-
+    if type(exec) == "string" then
+        if exec:lower():find("real") then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/ufg/EqHubUFG.lua"))()
+        else
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/ufg/lowlevelExctrs.lua"))()
+        end
+    end
 
 else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/Aimbot/Aimbot.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AtaScripts317/Equinox-Hub/refs/heads/main/Aimbot/Aimbot.lua", true))()
 end
